8fe2c0a6fde8be984389cef9878b3305581557b4
Ferran Figueredo <ferranfg@gmail.com>
2018-05-08 10:34:43 +0200

    Update payin-redsys.php

--- php/payin-redsys.php
+++ php/payin-redsys.php
@@ -16,28 +16,28 @@ try
     ]);
 
     $payIn = new Swagger\Client\Model\RedsysPayByWebPost([
-        'tag' => 'string',
+        'tag' => 'my-internal-id', // use your internal ID to identify this payment
         'save_card' => false,
         'card_id' => null,
         'credited_wallet_id' => $wallet->getId(), // wallet-create.php
         'statement_descriptor' => 'Test Name',
-        'url_ok' => 'http://api.lemonpay.me/ok',
-        'url_ko' => 'http://api.lemonpay.me/ko',
+        'success_url' => 'http://api.lemonpay.me/ok',
+        'cancel_url' => 'http://api.lemonpay.me/ko',
         'debited_funds' => $fund,
         'fees' => $fees
     ]);
 
-    $response = $payInApi->payInsRedsysRedsysPostPaymentByWeb($payIn);
+    $response = $payInApi->payInsRedsysRedsysPostPaymentByWeb('https://my-site.com/webhook', $payIn);
 
     /* ... */
     
     // MONTAR UNA VISTA HTML CON EL FORMULARIO:
     ?>
 
-    <form action="{{ $intent->getUrl() }}" method="POST">
-        <input type="hidden" name="Ds_SignatureVersion" value="{{ $intent->getDsSignatureVersion() }}" />
-        <input type="hidden" name="Ds_MerchantParameters" value="{{ $intent->getDsMerchantParameters() }}" />
-        <input type="hidden" name="Ds_Signature" value="{{ $intent->getDsSignature() }}" />
+    <form action="{{ $response->getUrl() }}" method="POST">
+        <input type="hidden" name="Ds_SignatureVersion" value="{{ $response->getDsSignatureVersion() }}" />
+        <input type="hidden" name="Ds_MerchantParameters" value="{{ $response->getDsMerchantParameters() }}" />
+        <input type="hidden" name="Ds_Signature" value="{{ $response->getDsSignature() }}" />
     </form>
 
     <script>
